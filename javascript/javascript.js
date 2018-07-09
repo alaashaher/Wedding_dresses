9da24a630ed7b56a7d7d854aea0cc8e170107456
unknown <alaashaher12@gmail.com>
2018-07-09 18:44:49 +0200

    media query

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,8 @@
 function clearContents(element) {
   element.value = '';
 }
+var remove_element = document.getElementById("remove_calss");
+if(navigator.userAgent.indexOf("Firefox") != -1) {
+         remove_element.className = "footer_body_firfox";
+}
+
